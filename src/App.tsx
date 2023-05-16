acb2d43a5f8d15f3ca860a9dc938b1e1a4774b55
Koki Takahashi <koki-takahashi@vcube.co.jp>
2023-05-16 11:05:06 +0900

    create favorite button

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,10 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import localforage from 'localforage';
+import { updateSourceFile } from 'typescript';
+import Button from '@mui/material/Button';
 
-
+//型の指定
 type repos_list={
   //favorite用のid
   id :number;
@@ -27,11 +29,13 @@ type repos_list={
   forks_count :number;
 }
 
+//引数用の指定
 type MyCardProps = {
   repos: repos_list;
 }
 
 
+//メインの関数
 function App() {
 
   //状態を持たせたコンポーネントuserNameを定義
@@ -40,6 +44,7 @@ function App() {
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [favorites, setFavorites] = useState<repos_list[]>([]);
 
+  //githubのリポジトリ検索を行うAPI
   const githubApi = async () => {
     try {
       const repoRes = await axios.get(`https://api.github.com/search/repositories?q=${keyWord}`);
@@ -51,26 +56,68 @@ function App() {
     }
   }
 
+  //ローカルストレージの中のfavoritesの一覧を取得
+  const getFavorites=()=>{
+    localforage.getItem<repos_list[]>('favorites')
+      .then((data) => {
+        if (data) {
+          setFavorites(data);
+          console.log('お気に入り情報を取得しました', data);
+        }
+      })
+      .catch((e) => {
+        console.error('お気に入り情報の取得に失敗しました', e);
+    });
+  }
+
+  
+  // お気に入りボタンがクリックされたときの関数
   const handleToggleFavorite=(repos: repos_list)=>{
+    //送られてきた情報のidだけ抽出してrepoIdに挿入
     const repoId = repos.id;
+    //.some()は、与えられた条件を満たす要素が配列内に存在するかどうかを確認するメソッド
+    //favoriteは引数名で、この場合はfavorites配列の要素。アロー関数の右側の式でその要素のidプロパティにアクセス。
     const isFavorite = favorites.some(favorite => favorite.id === repoId);
-  
-    let updatedFavorites: repos_list[]; // 変数を宣言
-  
+    
+    // 変数を宣言
+    let updatedFavorites: repos_list[]; 
+
     if (isFavorite) {
       // お気に入り解除
+      //filterは特定の条件を満たす要素を除外するメソッド
       updatedFavorites = favorites.filter(favorite => favorite.id !== repoId);
       setFavorites(updatedFavorites);
     } else {
       // お気に入り登録
+      //favoritesとお気に入りボタンを押したreposを合わせて登録
       updatedFavorites = [...favorites, repos];
       setFavorites(updatedFavorites);
     }
-  
     // お気に入り情報をWebストレージに保存
-    localforage.setItem('favorites', updatedFavorites).catch(err => {
+    localforage.setItem('favorites', updatedFavorites)
+    .then(() => {
+      console.log('お気に入り情報を保存しました');
+    })
+    .catch(err => {
       console.error('お気に入り情報の保存に失敗しました', err);
     });
+  }
+
+  //お気に入りに入っているか入っていないかを判定
+  const judgeFavorites=(repos: repos_list)=>{
+    const repoId = repos.id;
+    const isFavorite = favorites.some(favorite => favorite.id === repoId);
+    if(isFavorite){
+      return(
+        <Button sx={{ml:5}} onClick={() =>handleToggleFavorite(repos)} variant="contained">お気に入り解除</Button>
+      );
+    }else{
+      return(
+        <Button sx={{ml:5}} onClick={() =>handleToggleFavorite(repos)} variant="contained">お気に入り</Button>
+      );
+
+    }
+      
   }
 
   const MyCard= ({ repos }: MyCardProps) => {
@@ -96,30 +143,27 @@ function App() {
             <Typography>
               Fork数ss:{repos.created_at}
             </Typography>
-            <Typography>
-              <button onClick={() =>handleToggleFavorite(repos)}>お気に入り</button>
-            </Typography>
           </CardContent>
         </Card>
       </Box>
     );
   }
-
-  const handlePageClick = (data: { selected: number }) => {
-    setCurrentPage(data.selected);
-  };
-
-  //useEffectを用いて入力が終わったときに実行されるようにする。
-
+  
+  
+  //useEffectを用いてKeyWordの入力が終わったときに実行されるようにする。
   useEffect(() => {
     const timer = setTimeout(() => {
+      //githubのリポジトリを検索
       githubApi();
+      //ローカルのwebストレージからいいね一覧を取得
+      getFavorites();
     }, 500)
-
     return () => clearTimeout(timer)
   }, [keyWord])
-  return (
 
+
+  //メインのJSX
+  return (
     <div className="App">
       <TextField onChange = {e=> setKeyWord(e.target.value)} id="outlined-basic" label="Repositries" variant="outlined" />
       <h1>{keyWord}</h1>
@@ -130,7 +174,10 @@ function App() {
           .slice(currentPage*6,(currentPage+1)*6)
           .map((repos, index) => (
           <div key={index} className='w-25 h-300'>
-            <p>No.{index+(currentPage*5)}</p>
+            <p>
+              No.{index+(currentPage*5)}
+              {judgeFavorites(repos)}
+            </p>
             <MyCard repos = {repos} />
           </div>
           ))
